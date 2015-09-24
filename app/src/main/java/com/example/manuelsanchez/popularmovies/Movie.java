f34f5dd6c56f556a86f351f8dd6ead9d2fffee42
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-09-23 21:15:17 -0500

    Movie object for adapter

---
+++ app/src/main/java/com/example/manuelsanchez/popularmovies/Movie.java
@@ -0,0 +1,26 @@
+package com.example.manuelsanchez.popularmovies;
+
+/**
+ * Created by Manuel Sanchez on 9/23/15
+ */
+public class Movie {
+
+    private String movieTitle;
+    private String posterUrl;
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+}
